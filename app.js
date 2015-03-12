973229611c825a86ea17284b8a1098652f1f66a1
Yulia Markina <jmarkina@gmail.com>
2015-03-11 18:17:58 -0700

    replacing addrecipe.html and app.js

--- app.js
+++ app.js
@@ -18,6 +18,62 @@ var server = app.listen(3000, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log('http://localhost:3000/', host, port)
+});
 
-})
+
+// Get our form values. These rely on the "name" attributes
+// var recipeTitle = req.body.recipetitle;
+// var description = req.body.recdescription;
+// var time = req.body.cooktime;
+// var servings = req.body.servings;
+
+// Getting value from drop-down form
+// var selected_course = oForm.elements["course"].selectedIndex;
+
+// 	if(selected_course > 0)
+// 	{
+// 	   var selected_option_value = oForm.elements["course"].options[selected_course].value;
+// 	   var selected_option_text = oForm.elements["course"].options[selected_course].text;
+// 	}
+// 	else
+// 	{
+// 	   alert('Please select a course from the drop down list');
+// 	};
+
+//     // Set our collection
+//     var collection = db.get('mongodb://localhost/recipes');
+
+//     // Submit to the DB
+//     collection.insert({
+//         "title" : recipeTitle,
+//         "course" : selected_course,
+//         "description" : description,
+//         "time" : time,
+//         "servings" : servings
+//     }, function (err, doc) {
+//         if (err) {
+//             // If it failed, return error
+//             res.send("There was a problem adding the information to the database.");
+//         }
+//         else {
+//             // If it worked, set the header so the address bar doesn't still say /adduser
+//             res.location("mongodb://localhost/recipes");
+//             // And forward to success page
+//             res.redirect("index.html");
+//         }
+//     });
+// });
+
+// var recipeProvider = new RecipeProvider('localhost', 27017);
+
+// app.post('/recipes', function(req, res){
+//     recipeProvider.save({
+//         title: req.param('recipetitle'),
+//         course: req.param('selected_course'),
+//         description: req.param('recdescription'),
+//         time:  req.param('cooktime'),
+//         servings: req.param('servings')
+//     }, function(error, docs) {
+//             res.redirect('/')
+//     });
+// });
